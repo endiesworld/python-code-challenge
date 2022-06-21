421f7c21f9d0211d385354fb6e531638ab49d5b4
endiesworld <endiesworld@gmail.com>
2022-06-21 16:33:39 +0100

    Added solution to first and last position challenge

--- leetcode/first_last_position.py
+++ leetcode/first_last_position.py
@@ -1,5 +1,8 @@
 class Solution:
-    def searchRange(self, nums, target: int):
+    def __init__(self) -> None:
+        pass
+
+    def searchRange(self, nums, target):
         if len(nums) < 1:
             return [-1, -1]
         elif len(nums) == 1:
@@ -10,23 +13,50 @@ class Solution:
         def search_val(nums, target):
             mid = len(nums) // 2
 
-            if mid == 1:
+            if len(nums) == 1:
                 return 0 if nums[0] == target else 'No'
-            elif nums[mid] == target:
+            if nums[mid] == target:
                 return mid
-            elif nums[mid] > target:
+            if nums[mid] > target:
                 return search_val(nums[:mid], target)
             else:
                 try:
-                    return mid + search_val(nums[mid:], target)
+                    return (mid + search_val(nums[mid:], target))
                 except TypeError as e:
-                    return -1
+                    return 'No'
 
         ref_point = search_val(nums, target)
-
-        if ref_point == -1:
+        if (ref_point == -1) | (ref_point == 'No'):
             return [-1, -1]
-        store.append(re)
+        store.append(ref_point)
         first_part = nums[:ref_point]
-        second_part = nums[ref_point:]
-        # pointer =
+        second_part = nums[(ref_point+1):]
+
+        for index, data in enumerate(first_part):
+            real_index = -1*index - 1
+            if first_part[real_index] == target:
+                store.append(ref_point + real_index)
+            else:
+                break
+        for index, data in enumerate(second_part):
+            if data == target:
+                store.append(ref_point + index + 1)
+            else:
+                break
+
+        store = sorted(store)
+
+        result = [store[0], store[-1]]
+
+        return result
+
+
+solution = Solution()
+
+# print(solution.searchRange([1, 3], 1))
+# print(solution.searchRange([1, 4], 4))
+# print(solution.searchRange([-3, -2, -1], 0))
+# print(solution.searchRange([-3, -2, -2], -4))
+# print(solution.searchRange([2, 2], 3))
+# print(solution.searchRange([1, 2, 3], 3))
+print(solution.searchRange([1, 2, 3, 3, 3, 3, 4, 5, 9], 3))
---
+++ leetcode/test_case.py
@@ -0,0 +1,4 @@
+try:
+    print(2 + 2 + 'N0')
+except TypeError as e:
+    print('Error')
