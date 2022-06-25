b8db426204dcd0e8800955a68ccf2e5150258c37
endiesworld <endiesworld@gmail.com>
2022-06-25 18:07:27 +0100

    Updated maximum_position

--- leetcode/maximum_subarray.py
+++ leetcode/maximum_subarray.py
@@ -20,6 +20,41 @@ def maxSubArray(nums):
 
 
 item = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
-item = [1]
-item = [5, 4, -1, 7, 8]
-print(maxSubArray(item))
+# item = [-2, 1, -3, 4]
+# # # item = [1]
+# item = [5, 4, -1, 7, 8]
+
+x = [5, 4, 3, 7, 9]
+y = [-1, 7, 8]
+# print(maxSubArray(item))
+
+
+def texter(nums):
+    accum = nums[0]
+    start = 0
+    stop = 0
+    maxi = accum
+    skip = True
+
+    for index, data in enumerate(nums[1:]):
+
+        if (data > (accum + data)) & (data > maxi):
+            print(f'data: {data}')
+            start = index + 1
+            accum = data
+            maxi = data
+            skip = True
+            print(f'Accumulator is: {accum}')
+        elif (maxi <= (maxi + data)) & (skip):
+            print(f'Changed stop @ index {int(index) +1 }')
+            print(f'Data is: {data}')
+            # print(f'Accumulator is: {accum + data}')
+            stop = index + 2
+            # skip = True
+        else:
+            accum += data
+            skip = False
+    return (nums[start:stop], sum(nums[start:stop]))
+
+
+print(texter(item))
