c5079fb02f0522114f8d2c5cb3ae981c82db208f
endiesworld <endiesworld@gmail.com>
2022-06-27 00:59:58 +0100

    Added solution for stock timing challenge

---
+++ leetcode/stock_timing.py
@@ -0,0 +1,35 @@
+"""
+    You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0.
+"""
+from typing import List
+prices = [7, 1, 5, 3, 6, 4]
+# prices = [7, 6, 4, 3, 1]
+# prices = [7, 5, 9, 3, 4, 5]
+
+
+def timing(prices: List[int]) -> int:
+    buy = prices[0]
+    datas = prices[1:]
+    profit = 0
+    for data in datas:
+        if data < buy:
+            buy = data
+        elif (data - buy) > profit:
+            profit = data - buy
+    return profit
+
+
+print(timing(prices))
