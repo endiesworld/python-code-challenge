d07bea86abc3bc642f268fd13a4bacb4d5b5e8ed
endiesworld <endiesworld@gmail.com>
2022-07-10 12:19:41 +0100

    upgraded fibonacci

--- dynamic_programming/fibonacci.py
+++ dynamic_programming/fibonacci.py
@@ -1,12 +1,23 @@
-n = [4, 5, 35, 46]
+def exponential(a, n):
+    if n == 0:
+        return 1
+    return (a * exponential(a, n-1))
+
+
+# print(exponential(2, 8))
+n = [4, 5, 7, 35, 46]
+
+#  Version I fibonacci solution
 
 
 def fib(n):
-    if n == 1:
-        return 1
-    if n == 0:
-        return 0
+    store = {
+        1: 1,
+        0: 0
+    }
+    if n in store.keys():
+        return store[n]
     return fib(n-1) + fib(n-2)
 
 
-print(fib(n[3]))
+print(fib(n[2]))
