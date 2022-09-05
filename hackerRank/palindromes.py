22ea9a3fcc80292251a1bc6745617fedbef07ee2
endiesworld <endiesworld@gmail.com>
2022-09-05 02:36:14 +0100

    updated palindrum challenge

--- hackerRank/palindromes.py
+++ hackerRank/palindromes.py
@@ -21,4 +21,36 @@ def _palindrom(string, first_index, second_index):
 
 string = 'noeaon'
 string = 'level'
-print(palindrom(str.lower(string)))
+# print(palindrom(str.lower(string)))
+
+
+def longestPalindrome(s: str) -> str:
+    pal_length = 0
+    pal = ''
+    for index, char in enumerate(s):
+        base_str = s[index:]
+        if pal_length < len(base_str):
+            for sub_index, sub_char in enumerate(base_str):
+                sub_str = ''
+                if sub_index == 0:
+                    sub_str = base_str
+                else:
+                    sub_str = base_str[:-sub_index]
+                result = palindrom(sub_str)
+                if result:
+                    new_length = len(sub_str)
+                    if new_length > pal_length:
+                        pal_length = new_length
+                        pal = sub_str
+                    break
+        else:
+            break
+
+    return pal
+
+
+string = "cbbd"
+print(longestPalindrome(string))
+# print(string[:-0])
+# print(string[:-1])
+# print(string[:-2])
