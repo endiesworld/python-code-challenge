52e009dc2eea0c9c90fc420057e00fdb7b1d2fe4
endiesworld <endiesworld@gmail.com>
2022-09-09 01:30:06 +0100

    update on palindrom

--- hackerRank/palindromes.py
+++ hackerRank/palindromes.py
@@ -1,3 +1,7 @@
+from itertools import count
+from unittest import result
+
+
 def palindrom(string):
     first_index = 0
     second_index = len(string) - 1
@@ -27,30 +31,53 @@ string = 'level'
 def longestPalindrome(s: str) -> str:
     pal_length = 0
     pal = ''
-    for index, char in enumerate(s):
-        base_str = s[index:]
-        if pal_length < len(base_str):
-            for sub_index, sub_char in enumerate(base_str):
+    str_len = len(s)
+    main_count = 0
+    memory = []
+    while main_count < str_len:
+        base_str = s[main_count:]
+        if pal_length < (str_len - main_count):
+            sub_count = 0
+            balance_len = str_len - main_count
+            while sub_count < balance_len:
                 sub_str = ''
-                if sub_index == 0:
+                if sub_count == 0:
                     sub_str = base_str
                 else:
-                    sub_str = base_str[:-sub_index]
-                result = palindrom(sub_str)
+                    sub_str = base_str[:-sub_count]
+                passed_len = str_len - main_count - sub_count
+                result = small_palindrom(sub_str, passed_len)
                 if result:
+                    print('pal test passed')
                     new_length = len(sub_str)
                     if new_length > pal_length:
                         pal_length = new_length
                         pal = sub_str
                     break
+                sub_count += 1
+            main_count += 1
         else:
             break
 
     return pal
 
 
-string = "cbbd"
+def small_palindrom(s, str_len):
+    is_pal = True
+    count = 0
+    while count < str_len:
+        if s[count] != s[-1 - count]:
+            is_pal = False
+            break
+        count += 1
+    return is_pal
+
+
+# string = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+string = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabcaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
 print(longestPalindrome(string))
 # print(string[:-0])
 # print(string[:-1])
 # print(string[:-2])
+
+# print(small_palindrom(string))
