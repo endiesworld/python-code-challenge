74ba960ddf57c9642aeb63f889accf886a787a43
endiesworld <endiesworld@gmail.com>
2022-10-31 14:20:44 +0100

    updated maximum subarray

---
+++ leetcode/longest_substr.py
@@ -0,0 +1,96 @@
+def lengthOfLongestSubstring(s):
+    # str_len = len(s)
+    # store = {}
+    # length = 0
+    # maximum = 0
+
+    # for index, char in range(str_len):
+    #     length = 1
+    #     store[char] = char
+    #     for char2 in s[index+1:]:
+    #         if (char2 in store):
+    #             store = {}
+    #             break
+    #         length += 1
+    #         store[char2] = char2
+
+    #     maximum = max( maximum ,length)
+    #     print('maximum lenght:', maximum)
+    #     if maximum >= str_len - index:
+    #         break
+
+    # return maximum
+
+    #     store = {}
+    #     ref_index = 0
+    #     pointer = 0
+    #     maximum = 0
+
+    #     for index, char in enumerate(s):
+    #         if (char not in store):
+    #             store[char] = index
+    #             pointer = index
+    #         else:
+    #             print('**********************************')
+    #             print('store: ', store)
+    #             print('ref_index: ', ref_index)
+    #             print('pointer: ', pointer)
+    #             print('maximum: ', maximum)
+    #             maximum = max(maximum, (pointer - ref_index))
+    #             ref_index = store[char] + 1
+    #             store[char] = index
+
+    #     if(pointer == 0):
+    #         return 0
+
+    #     return max(maximum, ((pointer - ref_index)))
+    pass
+
+# print(lengthOfLongestSubstring("abcabcbb"))
+
+
+# def FindIntersection(strArr):
+
+#     # code goes here
+#     store = []
+#     myDict = {}
+#     second = {}
+#     for char in strArr[0].split(', '):
+#         myDict[char] = char
+#     for char in strArr[1].split(', '):
+#         if ((char in myDict) and (char not in second)):
+#             store.append(char)
+#             second[char] = char
+#     return store
+
+
+# # keep this function call here
+# print(FindIntersection(["1, 3, 4, 7, 13", "1, 2, 4, 13, 15"]))
+
+# print("1, 3, 4, 7, 13".split(', '))
+
+def MinWindowSubstring(strArr):
+
+    # code goes here
+    store = {}
+    index_ref = []
+    ref_store = {}
+    last_index = -1
+    new_string = ''
+    empty = True
+    for char in strArr[1]:
+        ref_store[char] = char
+    ref_store_1 = ref_store
+
+    for index, char in enumerate(strArr[0]):
+        if ((char in ref_store_1) and (char not in store)):
+            print(char)
+
+    for index in index_ref:
+        new_string += strArr[0][index]
+
+    return new_string
+
+
+# keep this function call here
+print(MinWindowSubstring(["ahffaksfajeeubsne", "jefaa"]))
--- leetcode/maximum_subarray.py
+++ leetcode/maximum_subarray.py
@@ -19,15 +19,6 @@ def maxSubArray(nums):
     return (store, max_val)
 
 
-item = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
-item = [-2, 1, -3, 4]
-item = [3, -5, 1, 9, -2, 4]
-item = [8, -19, 5, -4, 20]
-# item = [-1, -2]
-# item = [5, 4, -1, 7, 8]
-item = [5, 4, -1]
-
-
 def texter(nums):
     accum = nums[0]
     maxi = accum
@@ -56,7 +47,75 @@ def texter(nums):
 #     result = nums[0] + texter_recursive(nums[1:])
 #     return result
 
-print(texter(item))
+# print(texter(item))
 
 
 # print(texter_recursive(item))
+# item = [-1, -1, -2, -2]
+
+
+# def slidingWindow(items):
+#     maximum = items[0]
+#     start = 0
+#     stop = 0
+#     operation_max = maximum
+#     for index, data in enumerate(items):
+#         if((index - 1) >= 0):
+#             if ((data > (operation_max + data)) and data > operation_max):
+#                 start = index
+#                 stop = index
+#                 maximum = data
+#                 operation_max = data
+#                 print('data is greater, ', data)
+#             elif (maximum + data > operation_max):
+#                 stop = index
+#                 maximum += data
+#                 operation_max = maximum
+#             else:
+#                 maximum += data
+#     print(f'start: {start},  stop: {stop}')
+#     return sum(items[start: stop+1])
+
+item = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+# item = [-2, 1, -3, 4]
+# item = [3, -5, 1, 9, -2, 4]
+# item = [8, -19, 5, -4, 20]
+# item = [-1, -2]
+# item = [5, 4, -1, 7, 8]
+# item = [5, 4, -1]
+# item = [-1, -1, -2, -2]
+item = [5, 4, -1]
+# item = [-1, -1, -2, -2]
+# item = [8, -19, 5, -4, 20]
+# item = [-2, 1]
+# item = [-2, -1]
+
+
+def slidingWindow(nums):
+    maximum = nums[0]
+    start = 0
+    stop = 0
+    running_sum = maximum
+    for index, data in enumerate(nums):
+        if((index - 1) >= 0):
+            if((data > (running_sum + data)) and (data > running_sum)):
+                maximum = data
+                running_sum = maximum
+                start = index
+                stop = index
+            elif(running_sum + data > maximum):
+                stop = index
+                maximum = running_sum + data
+                running_sum = maximum
+            elif(running_sum + data < 0):
+                running_sum = 0
+                start = index + 1
+            else:
+                running_sum += data
+    start = min(stop, start)
+    print('Array sum: ', sum(nums[start: stop + 1]))
+    print(f'start: {start},  stop: {stop}')
+    return maximum
+
+
+print(slidingWindow(item))
