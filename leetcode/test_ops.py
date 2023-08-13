6f88a9e25c4198e14ee91f19c51d8a3dd8695db4
endiesworld <endiesworld@gmail.com>
2023-08-13 14:51:49 +0100

    feat: Added update on python list and tuple

---
+++ leetcode/test_ops.py
@@ -0,0 +1,16 @@
+
+def main(*args, **kwargs):
+    my_list_1 = [3,6, "hello", "world"]
+    my_tuple_1 = (1,3,5, "hello", "world")
+    
+    print("my_list_1 contains: ", end=" ")
+    for data in my_list_1:
+        print(f"{data},", end=" ")
+        
+    print("\nmy_tuple_1 contains: ", end=" ")
+    for data in my_tuple_1:
+        print(f"{data},", end=" ")
+        
+
+if __name__ == '__main__':
+    main()
