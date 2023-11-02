32da8d851ad34a5225bd782a02ef9bdd0de6d5c0
endiesworld <endiesworld@gmail.com>
2023-11-02 16:50:37 +0100

    feat: Added context manager class for sqlite database operation

--- leetcode/db_connections/sqlite_connection/add_multiple_data.py
+++ leetcode/db_connections/sqlite_connection/add_multiple_data.py
@@ -1,21 +1,34 @@
 import sqlite3
 
+class Database:
+    def __init__(self, path: str):
+        self.path = path
+
+    def __enter__(self):
+        self.connection = sqlite3.connect(self.path)
+        self.cursor = self.connection.cursor()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+    	if exc_type is not None:
+            print(f'an error occurred: {exc_val}')
+
 DB = """movies.db"""
 
-MOVIES = [(2, 'Things Fall Apart', 'Acheber', 1989),
-          (3, 'Blame the gods', 'Yinka', 1992),
-          (4, 'Water an Enemy', 'Fola', 1996)]
+MOVIES = [(9, 'Asharami synergy', 'Tonye Cole', 2003),
+          (10, 'Ikeja Electric', 'Tope Shonubi', 2015),
+          (11, 'ATT Ikeja', 'Adeyonyi Moroti', 2018)]
 
 INSERT_MOVIES = """ INSERT INTO Movies Values(?,?,?, ?)"""
 SELECT_MOVIES = """SELECT * FROM Movies"""
 
-connection = sqlite3.connect(DB)
 
-cursor = connection.cursor()
-cursor.executemany(INSERT_MOVIES, MOVIES)
-cursor.execute(SELECT_MOVIES)
+with Database(DB) as db:
+    db.cursor.executemany(INSERT_MOVIES, MOVIES)
+    db.connection.commit()
+    db.cursor.execute(SELECT_MOVIES)
 
-print(cursor.fetchall())
+    print(db.cursor.fetchall())
 
-connection.commit()
-connection.close()
+    
+   
