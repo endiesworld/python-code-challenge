3a3bd71ccecbcd4e92491ff063cbdf234750af93
endiesworld <endiesworld@gmail.com>
2023-11-11 16:30:33 +0100

    feat: Added enums to class operations'

---
+++ leetcode/advanced_classes/enums_ops.py
@@ -0,0 +1,22 @@
+from enum import Enum, unique
+
+#  You can prevent dublicate value by using the "@unique" 
+class Fruit(Enum):
+    APPLE = 1
+    BANANA = 2
+    ORANGE = 3
+    TOMATO = 4
+    
+    
+
+def main():
+    print(Fruit.APPLE)
+    print(type(Fruit.APPLE))
+    print(repr(Fruit.BANANA))
+    
+    # Enums have both name and value
+    print(f"The name of this enum: {Fruit.APPLE.name}, and value is : {Fruit.APPLE.value}")
+    
+
+if __name__ == '__main__':
+    main()
