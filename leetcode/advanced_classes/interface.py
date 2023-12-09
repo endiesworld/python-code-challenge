52d77b5ef8f8f7babaf122cfec0f5cb2e591f97f
endiesworld <endiesworld@gmail.com>
2023-12-09 10:15:59 +0100

    feat: added comment to talk about the meaning and the use of interface

--- leetcode/advanced_classes/interface.py
+++ leetcode/advanced_classes/interface.py
@@ -1,4 +1,8 @@
-from abc import ABC, abstractmethod
+"""
+Implementing an interface allows a class to become more formal about the behavior it promises to provide. 
+Interfaces form a contract between the class and the outside world.
+If your class claims to implement an interface, all methods defined by that interface must appear in its source code
+"""
 
 from abc import ABC, abstractmethod
 
