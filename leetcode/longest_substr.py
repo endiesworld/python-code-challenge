bb08897966bcd8f14b7dc1490fade35201985213
endiesworld <endiesworld@gmail.com>
2024-02-12 04:16:09 +0100

    Added patrtial solution to sliding window challange

--- leetcode/longest_substr.py
+++ leetcode/longest_substr.py
@@ -69,31 +69,104 @@ def lengthOfLongestSubstring(s):
 
 # print("1, 3, 4, 7, 13".split(', '))
 
-def MinWindowSubstring(strArr):
+# def MinWindowSubstring(strArr):
 
-    # code goes here
-    store = {}
-    index_ref = [strArr[0]]
-    ref_store = {}
-    last_index = -1
-    new_string = ''
-    empty = True
-    start = 0
-    stop = 0
-    for char in strArr[1]:
-        ref_store[char] = char
-    ref_store_1 = ref_store
+#     # code goes here
+#     store = {}
+#     index_ref = [strArr[0]]
+#     ref_store = {}
+#     last_index = -1
+#     new_string = ''
+#     empty = True
+#     start = 0
+#     stop = 0
+#     for char in strArr[1]:
+#         ref_store[char] = char
+#     ref_store_1 = ref_store
 
-    for index, char in enumerate(strArr[0]):
-        if(index - 1 >= 0):
-            if (char in ref_store_1):
-                index_ref.append(char)
+#     for index, char in enumerate(strArr[0]):
+#         if(index - 1 >= 0):
+#             if (char in ref_store_1):
+#                 index_ref.append(char)
 
-    for index in index_ref:
-        new_string += strArr[0][index]
+#     for index in index_ref:
+#         new_string += strArr[0][index]
 
-    return new_string
+#     return new_string
 
 
 # keep this function call here
-print(MinWindowSubstring(["ahffaksfajeeubsne", "jefaa"]))
+
+# def MinSubstring(strArr):
+#     len_1 = len(strArr[0])
+#     len_2 = len(strArr[1])
+#     str_1 = strArr[0]
+#     str_2 = strArr[1]
+#     sub_str = ''
+#     temp_substr = ''
+#     match_str = ''
+#     comp_str = ''
+    
+#     if len_1 > len_2: 
+#         match_str = str_2 
+#         comp_str = str_1
+#     else: 
+#         match_str = str_1
+#         comp_str = str_2
+#     print("match_str: ", match_str)
+    
+#     for str_data in match_str:
+#         temp_substr = temp_substr + str_data
+#         if temp_substr in comp_str:
+#             if len(temp_substr) > len(sub_str) :
+#                 sub_str = temp_substr
+#                 print("substring: ", sub_str)
+#         else:
+#             temp_substr = str_data
+            
+#     return sub_str
+    
+    
+def MinSubstring(s, t):
+    len_s = len(s)
+    min_window_1 = ''
+    len_t = len(t)
+    window = True
+    if len_t > len_s:
+        return ''
+    
+    for i in range(len_s):
+        test_window = s[i:]
+        if (len_s - i >= len_t):
+            for data in t:
+                if data not in test_window:
+                    window = False
+                    break
+            if window:
+                min_window_1 = test_window
+    print("min_window_1: ", min_window_1)
+    s = min_window_1
+    min_window_2 = s
+    window = True
+    while len_s > 0:
+        test_window = s[:len_s]
+        if (len_s >= len_t):
+            for data in t:
+                if data not in test_window:
+                    window = False
+                    break
+            if window:
+                min_window_2 = test_window
+        len_s -=1
+    print("min_window_1: ", min_window_1)
+    if len(min_window_2) < len(min_window_1):
+        for data in t:
+            if t.count(data) != min_window_2.count(data):
+                return min_window_1
+        return min_window_2
+    return min_window_1
+
+print(MinSubstring("bbaac", "aba"))
+print(MinSubstring("ADOBECODEBANC", "ABC"))
+print(MinSubstring("A", "A"))
+print(MinSubstring("acbbaca", "aba"))
--- leetcode/merge_sorted_array.py
+++ leetcode/merge_sorted_array.py
@@ -21,14 +21,46 @@ def main(nums1, m, nums2 , n, *args, **kwargs):
             container.append(nums2[n_pointer + index])
             
     return container
+
+
+def mergeTwoLists(list1, list2):
+        result = []
+        counter = 0
+        len_1 = len(list1)
+        len_2 = len(list2)
+        pos_1 = 0
+        pos_2 = 0
+
+        for i in range(len_1 + len_2):
+            if list1[pos_1] < list2[pos_2]:
+                result.append(list1[pos_1])
+                pos_1 += 1
+            else:
+                result.append(list2[pos_2])
+                pos_2 += 1
+            if (pos_1 == len_1 ):
+                while pos_2 < len_2:
+                    result.append(list2[pos_2])
+                    pos_2 += 1
+                break
+            if (pos_2 == len_2 ):
+                while pos_1 < len_1:
+                    result.append(list1[pos_1])
+                    pos_1 += 1
+                break
+
+        return result
+
+print(mergeTwoLists([1,2,4,5,9,10,11], [1,3,4,7,8,20]))
+
     
-if __name__ == '__main__':
-    arr1 = sys.argv[1]
-    n = int(sys.argv[2])
-    arr2 = sys.argv[3]
-    m = int(sys.argv[4])
+# if __name__ == '__main__':
+#     arr1 = sys.argv[1]
+#     n = int(sys.argv[2])
+#     arr2 = sys.argv[3]
+#     m = int(sys.argv[4])
     
-    arr1 = [int(data) for data in arr1 if data.isdigit()]
-    arr2 = [int(data) for data in arr2 if data.isdigit()]
+#     arr1 = [int(data) for data in arr1 if data.isdigit()]
+#     arr2 = [int(data) for data in arr2 if data.isdigit()]
     
-    print(main(arr1, n, arr2, m))
+#     print(main(arr1, n, arr2, m))
